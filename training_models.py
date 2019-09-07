d6b594fa82d7d64be1dfc56a36281516ef0ec818
Jap-Leen <jap.jolly@gmail.com>
2019-09-07 15:08:38 +0530

    added models

--- training_models.py
+++ training_models.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import pickle
 from pymongo import MongoClient 
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer, WordNetLemmatizer
@@ -8,7 +9,10 @@ from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import MultinomialNB, GaussianNB
+from sklearn.linear_model import SGDClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 
 # Getting stopwords set from english language
 STOPWORDS = set(stopwords.words('english'))
@@ -37,8 +41,8 @@ def RemoveStopwords(text):
 def PreProcessing(feature):
 	df[feature] = df[feature].apply(ConvertToString)
 	df[feature] = df[feature].str.lower()
-	df[feature] = df[feature].apply(Stemming)
-	df[feature] = df[feature].apply(Lemmatization)
+	# df[feature] = df[feature].apply(Stemming)
+	# df[feature] = df[feature].apply(Lemmatization)
 	df[feature] = df[feature].apply(RemoveStopwords)
 
 # Setting up MongoDB client 
@@ -67,12 +71,30 @@ y = df.flair
 # Splitting data into training and testing sets
 x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.25, random_state=10)
 
-def NaiveBayes(x_train,y_train,x_test,y_test):
+def MultinomialNaiveBayes(x_train,y_train,x_test,y_test):
 	nb = Pipeline([('vect', CountVectorizer()), ('tfidf', TfidfTransformer()), ('clf', MultinomialNB())])
 	nb.fit(x_train,y_train)
 	y_pred = nb.predict(x_test)
-	print("Naive Bayes: "+str(accuracy_score(y_pred,y_test)))
+	print("Naive Bayes accuracy_score: "+str(accuracy_score(y_pred,y_test)))
 
-NaiveBayes(x_train,y_train,x_test,y_test)
+def LinearSVM(x_train,y_train,x_test,y_test):
+	sgd = Pipeline([('vect',CountVectorizer()),('tfidf',TfidfTransformer()),('clf', SGDClassifier(loss='hinge', penalty='l2',alpha=1e-3, random_state=42, max_iter=5, tol=None))])
+	sgd.fit(x_train,y_train)
+	y_pred = sgd.predict(x_test)
+	print("Linear SVM accuracy_score: "+str(accuracy_score(y_pred,y_test)))
+	model = 'finalized_model.sav'
+	pickle.dump(sgd, open(model, 'wb'))
+
+def LogisticRegressionC(x_train, y_train, x_test, y_test):
+	logistic_reg = Pipeline([('vect', CountVectorizer()),('tfidf', TfidfTransformer()),('clf', LogisticRegression(n_jobs = 1, C = 1e5))])
+	logistic_reg.fit(x_train,y_train)
+	y_pred = logistic_reg.predict(x_test)
+	print("Logistic Regression accuracy_score: "+str(accuracy_score(y_pred,y_test)))
+
+# Training different models
+MultinomialNaiveBayes(x_train,y_train,x_test,y_test)
+LinearSVM(x_train,y_train,x_test,y_test)
+LogisticRegressionC(x_train,y_train,x_test,y_test)
+
 
 
