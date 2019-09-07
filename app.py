0fde22e962f1ae012866f015626b6e5058ea3702
Jap-Leen <jap.jolly@gmail.com>
2019-09-07 23:27:12 +0530

    modified files for deployment

--- app.py
+++ app.py
@@ -15,5 +15,9 @@ def flairDetect():
 	return render_template('index.html',flair=flair)
 
 port = int(os.getenv('VCAP_APP_PORT', 5000))
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=port)
+
+# if __name__ == "__main__":
+#     app.run()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=port)
